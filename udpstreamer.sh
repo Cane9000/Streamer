e785daf93e742bc9861993e7d6c69adf105d6d3d
Cane9000 <andrewcane@gmail.com>
2019-12-22 22:50:57 +0000

    Commit

--- udpstreamer.sh
+++ udpstreamer.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-#PLATFORM SCRIPT
+#UDP/RTP TEST
 
-#USE DOS2UNIX application to convert config file - removing carriage returns, etc 
-dos2unix www-config.txt
-
-#READ CONGIF
-. /var/www/html/www-config.txt;
-#PLAYLIST GENERATION
-ls /media/pi/9601-27A0/video/E*.mp4 -t > flist.txt;sed -i s/^/file\ \'/g flist.txt; sed -i  s/.mp4/.mp4\'/g /var/www/html/flist.txt
-x=0
->playlist.txt
-while [ $x -le $(($LOOP-1)) ]; do cat flist.txt>>playlist.txt; x=$(( $x + 1 ))
-done
 
 #STREAMING
-ffmpeg     -re -f concat -safe 0 -i playlist.txt -vcodec libx264 -ac 2 -ar 44100 -s $W:$H  -b $BIT -f mpegts udp://192.168.1.69:1234
+ffmpeg     -re -i /media/pi/9601-27A0/video/E3.mp4 -vcodec libx264 -ac 2 -ar 44100 -s 480:320 -b 400k -f mpegts udp://192.168.1.69:1234
 
 
